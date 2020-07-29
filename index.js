4574feceade099a2185a70ee360800882a8aef4b
DJ Viking <anders.swedishviking@gmail.com>
2020-07-29 01:19:04 -0400

    finishing up the user input functions, moving on to creating functions that will generate the markdown prior to writing the file

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
+//***DONE GIVEN a command-line application that accepts user input
+//***DONE WHEN I am prompted for information about my application repository
+//SOON THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//***DONE WHEN I enter my project title
+//SOON THEN this is displayed as the title of the README
+//***DONE WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+//SOON THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+//***DONE WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+//***DONE WHEN I enter my GitHub username
+//SOON THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+//DONE*** WHEN I enter my email address
+//SOON THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 
@@ -31,7 +31,7 @@
 // function init() {
 
 // }
-
+const licenseChoicesArray = ['MIT', 'ISC', 'Academic Free License v3.0', 'Apache license 2.0', 'Artistic license 2.0', 'Boost Software License 1.0', 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', 'Creative Commons license family', 'Creative Commons Zero v1.0 Universal', 'Creative Commons Attribution 4.0', 'Creative Commons Attribution Share Alike 4.0', 'Do What The F*** You Want To Public License', 'Educational Community License v2.0', 'Eclipse Public License 1.0', 'European Union Public License 1.1', 'European Union Public License 1.1', 'GNU General Public License family', 'GNU General Public License v2.0', 'GNU General Public License v3.0', 'GNU Lesser General Public License family', 'GNU Lesser General Public License v2.1', 'GNU Lesser General Public License v3.0', 'LaTeX Project Public License v1.3c', 'Microsoft Public License', 'Mozilla Public License 2.0', 'Open Software License 3.0', 'PostgreSQL License', 'SIL Open Font License 1.1', 'University of Illinois/NCSA Open Source License', 'The Unlicense', 'zLib License']
 // // function call to initialize program
 // init();
 
@@ -83,17 +83,11 @@ promptUser = () => {
             message: 'Please provide a screenshot usage example \n(provide the relative path URL of where the image is located in your project directory):',
             default: false
         },
-        {
-            type: 'input',
-            name: 'license',
-            message: 'Which license would you like to add to this project?\n For info on a list of licenses go to this page -> \n https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/licensing-a-repository',
-            default: false //for this condition create the license section anyway with the text "Need help choosing a license? check out this link for more information on which license to choose: (https://choosealicense.com/)"
-            //if true create a badge with the license type at the top of the page.
-        },
+        
         {
             type: 'input',
             name: 'email',
-            message: 'If any end-users of your application were to have any additional questions, which email should they contact? \n Please provide your email:',
+            message: 'If any end-users of your application were to have any additional questions, \nwhich email should they contact? \n Please provide your email:',
             validate: emailInput => {
                 if (emailInput){
                     return true;
@@ -125,14 +119,14 @@ promptContrib = readmeData => {
     }
     console.log(`
 =================
-Add a contributor
+Add a Contributor
 =================
     `)
     return inquirer.prompt ([
         {
             type: 'input',
-            name: 'contributors',
-            message: 'Provide a contributor name to this project (if any) \n separating the contributor name in this current format - firstname lastname:',
+            name: 'contributor',
+            message: 'Provide a contributor name to this project (if any) \n separating the contributor name in this current format - firstname lastname: \n If you are the only contributor just put your name:',
             default: false
         },
         {
@@ -153,12 +147,61 @@ Add a contributor
 
 }
 
+//create function for asking user if they want a license or not. 
+//if confirmed then let them choose which one
+promptLicense = readmeData => {
+    console.log(`
+    =============
+    Add a License
+    =============
+        `)
+    if (!readmeData.licenseArray){
+        readmeData.licenseArray = [];
+        console.log(readmeData);
+    }
+    return inquirer.prompt ([
+        {
+            type: 'confirm',
+            name: 'licenseConfirm',
+            message: 'Would you like to add a license to your project?',
+            validate: licenseConfirm => {
+                if (licenseConfirm) {
+                    return true;
+                } else {
+                    return false;//if this condition happens create the license section anyway and add the link to point user towards learning about which license to choose
+                }
+            }
+        },  
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Which license would you like to add to this project?\n For info on which license to choose go to this page -> \n https://choosealicense.com/ \n Please keep in mind your readme will only reflect the FIRST choice you select',
+            choices: licenseChoicesArray,
+            when: ({licenseConfirm}) => licenseConfirm
+            //if true create a badge with the license type at the top of the page.
+        }
+
+    ])
+    .then(licenseData => {
+        console.log(licenseData);
+        readmeData.licenseArray.push(licenseData);
+        return readmeData;
+    });
+}
+
+
 promptUser()
-.then(answers => {
-    console.log(answers);
-    return answers;
+.then(object1 => {
+    console.log(object1);
+    return object1;
 })
 .then(promptContrib)
-.then(contributors => {
-    console.log(contributors);
+.then(object2 => {
+    console.log(object2);
+    return object2;
+})
+.then(promptLicense)
+.then(object3 => {
+    console.log(object3);
+
 })
